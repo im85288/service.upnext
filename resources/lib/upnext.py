4d011681b80ad8023c533ade78b62c7734872ded
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2020-08-19 21:19:51 +1000

    Remove duplicate SIGNAL postfix (already added in util event method)

--- resources/lib/upnext.py
+++ resources/lib/upnext.py
@@ -8,7 +8,7 @@ from utils import event
 def send_signal(sender, next_info):
     """Helper function for plugins to send up next data to UpNext"""
     event(
-        sender=sender + '.SIGNAL',
+        sender=sender,
         message='upnext_data',
         data=next_info,
         encoding='base64'
