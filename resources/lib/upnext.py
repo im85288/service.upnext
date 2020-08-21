8ed92c1bb7479deb959d24f936679ce76b76c1c9
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2020-08-21 14:36:23 +1000

    Update upnext module to simplify addon integration
    Addons can now send ListItem or InfoTagVideo objects to Up Next.
    This reduces barrier for integration, only requiring the following for basic functionality:
    
    xbmcplugin.setResolvedUrl(handle, True, listItem)
    
    try:
        import upnext
    except Exception as e:
        utils.log('Up Next addon not installed: %s' % e)
        return
    
    upnext_info = dict(
        current_episode=listItem,
        play_url=url
    )
    
    upnext.send_signal(xbmcaddon.Addon().getAddonInfo('id'), upnext_info)

--- resources/lib/upnext.py
+++ resources/lib/upnext.py
@@ -2,14 +2,74 @@
 # GNU General Public License v2.0 (see COPYING or https://www.gnu.org/licenses/gpl-2.0.txt)
 
 from __future__ import absolute_import, division, unicode_literals
-from utils import event
+from xbmc import InfoTagVideo
+from xbmcgui import ListItem
+from utils import event, get_int, log as ulog
 
 
-def send_signal(sender, next_info):
+def log(msg, level=2):
+    ulog(msg, name=__name__, level=level)
+
+
+def send_signal(sender, upnext_info):
     """Helper function for plugins to send up next data to UpNext"""
+    # Exit if not enough addon information provided
+    if ('current_episode' not in upnext_info
+        or ('play_url' not in upnext_info
+            and 'play_info' not in upnext_info)):
+        log('Sending invalid Up Next data: %s' % upnext_info, 1)
+        return
+
+    # Extract ListItem or InfoTagVideo details for use by Up Next
+    for key, val in upnext_info.items():
+        thumb = ''
+        fanart = ''
+        if isinstance(val, ListItem):
+            thumb = val.getArt('thumb')
+            fanart = val.getArt('fanart')
+            val = val.getVideoInfoTag()
+
+        if isinstance(val, InfoTagVideo):
+            upnext_info[key] = dict(
+                episodeid=val.getDbId(),
+                # Use show title as substitute for missing ListItem tvshowid
+                tvshowid=val.getTVShowTitle() or -1,
+                title=val.getTitle(),
+                art={
+                    'thumb': thumb,
+                    'tvshow.fanart': fanart,
+                },
+                season=val.getSeason(),
+                episode=val.getEpisode(),
+                showtitle=val.getTVShowTitle(),
+                plot=val.getPlotOutline() or val.getPlot(),
+                playcount=val.getPlayCount(),
+                rating=val.getUserRating() or int(val.getRating()),
+                firstaired=(
+                    val.getFirstAired()
+                    or val.getPremiered()
+                    or val.getYear()
+                ),
+                runtime=val.getDuration()
+            )
+
+    # If next episode information is not provided, fake it
+    if not upnext_info.get('next_episode'):
+        episode = upnext_info['current_episode']
+        episode['episodeid'] = -1
+        episode['title'] = 'Next episode'
+        episode['art'] = {}
+        episode['episode'] = get_int(episode, 'episode') + 1
+        episode['plot'] = ''
+        episode['playcount'] = 0
+        episode['rating'] = 0
+        episode['firstaired'] = ''
+        episode['runtime'] = 0
+        upnext_info['next_episode'] = episode
+
     event(
         sender=sender,
         message='upnext_data',
-        data=next_info,
+        data=upnext_info,
         encoding='base64'
     )
