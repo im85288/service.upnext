1309e6b4bc2e56caab1d2e4b52eb36106bad121b
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2020-08-25 15:20:32 +1000

    Cherry picked from:
    2882e1a4b170a1b7ba65acefa919a5f2a392d9de
    3fc4c0f02740ca827af9af5a36eb1ec70508ca38
    b8edb738ff0d6fcd897d41de1d19aedb3361aab5
    07f69d37260a139b90370180b11ef137ed83f4a7
    66c8c1f85f6426ba3a788a342cc625ff96675009
    4d011681b80ad8023c533ade78b62c7734872ded
    8ed92c1bb7479deb959d24f936679ce76b76c1c9
    950ff471e377195ed6adda1415b7d8711bf2ca79
    ec0bc4153c3bf25d3d22a53d9abe4385ac00613d
    2ac1ac680bfa49115508c9786da0a88cc61325bb
    f586a14bf8ef20054d16657a08990f65d640127e

--- resources/lib/upnext.py
+++ resources/lib/upnext.py
@@ -2,9 +2,85 @@
 # GNU General Public License v2.0 (see COPYING or https://www.gnu.org/licenses/gpl-2.0.txt)
 
 from __future__ import absolute_import, division, unicode_literals
-from utils import event
+from xbmc import InfoTagVideo
+from xbmcgui import ListItem
+from utils import event, log as ulog
 
 
-def send_signal(sender, next_info):
+def log(msg, level=2):
+    ulog(msg, name=__name__, level=level)
+
+
+def send_signal(sender, upnext_info):
     """Helper function for plugins to send up next data to UpNext"""
-    event(sender=sender + '.SIGNAL', message='upnext_data', data=next_info, encoding='base64')
+    # Exit if not enough addon information provided
+    info_check = (
+        (upnext_info.get('play_url') or upnext_info.get('play_info'))
+        and upnext_info.get('current_episode')
+    )
+    if not info_check:
+        log('Sending invalid Up Next info: %s' % upnext_info, 1)
+        return
+
+    # Extract ListItem or InfoTagVideo details for use by Up Next
+    for key, val in upnext_info.items():
+        thumb = ''
+        fanart = ''
+        if isinstance(val, ListItem):
+            thumb = val.getArt('thumb')
+            fanart = val.getArt('fanart')
+            val = val.getVideoInfoTag()
+
+        if isinstance(val, InfoTagVideo):
+            upnext_info[key] = dict(
+                episodeid=val.getDbId(),
+                # Use show title as substitute for missing ListItem tvshowid
+                tvshowid=val.getTVShowTitle() or -1,
+                title=val.getTitle(),
+                art={
+                    'thumb': thumb,
+                    'tvshow.fanart': fanart,
+                },
+                season=val.getSeason(),
+                episode=val.getEpisode(),
+                showtitle=val.getTVShowTitle(),
+                plot=val.getPlotOutline() or val.getPlot(),
+                playcount=val.getPlayCount(),
+                rating=val.getUserRating() or int(val.getRating()),
+                firstaired=(
+                    val.getFirstAired()
+                    or val.getPremiered()
+                    or val.getYear()
+                ),
+                runtime=val.getDuration()
+            )
+
+    # If next episode information is not provided, fake it
+    if not upnext_info.get('next_episode'):
+        episode = upnext_info['current_episode']
+        episode['episodeid'] = -1
+        episode['art'] = {}
+        # Next provided episode may not be the next consecutive episode so we
+        # can't assume that the episode can simply be incremented, instead set
+        # title to indicate the next episode in the Up Next popup
+        # from utils import get_int
+        # episode['episode'] = get_int(episode, 'episode') + 1
+        # TODO: Change to localised string variable for translation purposes
+        episode['title'] = 'Next episode'
+        # Change season and episode info to empty string to avoid episode
+        # formatting issues ("S-1E-1") in Up Next popup
+        episode['season'] = ''
+        episode['episode'] = ''
+        episode['plot'] = ''
+        episode['playcount'] = 0
+        episode['rating'] = 0
+        episode['firstaired'] = ''
+        episode['runtime'] = 0
+        upnext_info['next_episode'] = episode
+
+    event(
+        sender=sender,
+        message='upnext_data',
+        data=upnext_info,
+        encoding='base64'
+    )
