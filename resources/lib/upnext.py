05ffcb2a2a76a301ecaee9d6cec23451e02ab8b0
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2020-08-25 11:44:10 +1000

    Python 2/3 compatibility, fix issues with relative import
    (cherry picked from commit 07f69d37260a139b90370180b11ef137ed83f4a7)

--- resources/lib/upnext.py
+++ resources/lib/upnext.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+# GNU General Public License v2.0 (see COPYING or https://www.gnu.org/licenses/gpl-2.0.txt)
+
+from __future__ import absolute_import, division, unicode_literals
 from utils import event
+
 
 def send_signal(sender, next_info):
     """Helper function for plugins to send up next data to UpNext"""
