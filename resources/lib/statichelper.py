77fb2317b54f733fdbc8d65ef2834a2583d849ea
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2020-09-02 13:42:29 +1000

    Remove uneccesary test change that breaks Python 3 versions older than 3.5 which do not accept binary input for json.loads

--- resources/lib/statichelper.py
+++ resources/lib/statichelper.py
@@ -6,18 +6,15 @@ from __future__ import absolute_import, division, unicode_literals
 import sys
 
 
-IS_PYTHON2 = sys.version_info.major == 2
-
-
 def to_unicode(text, encoding='utf-8', errors='strict'):
     """Force text to unicode"""
-    if IS_PYTHON2 and isinstance(text, (bytes, str)):
+    if isinstance(text, bytes):
         return text.decode(encoding, errors)
     return text
 
 
 def from_unicode(text, encoding='utf-8', errors='strict'):
     """Force unicode to text"""
-    if IS_PYTHON2 and isinstance(text, unicode):  # noqa: F821; pylint: disable=undefined-variable,useless-suppression
+    if sys.version_info.major == 2 and isinstance(text, unicode):  # noqa: F821; pylint: disable=undefined-variable,useless-suppression
         return text.encode(encoding, errors)
     return text
