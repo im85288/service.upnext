f1e810a8ec22f5f4421a11d3e68ef0d60968c368
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2021-01-25 02:41:55 +1100

    Use tvshowid if present as a ListItem property

--- resources/lib/upnext.py
+++ resources/lib/upnext.py
@@ -24,16 +24,20 @@ def send_signal(sender, upnext_info):
     for key, val in upnext_info.items():
         thumb = ''
         fanart = ''
+        tvshowid = ''
         if isinstance(val, xbmcgui.ListItem):
             thumb = val.getArt('thumb')
             fanart = val.getArt('fanart')
+            tvshowid = val.getProperty('tvshowid')
+            if tvshowid == '-1':
+                tvshowid = ''
             val = val.getVideoInfoTag()
 
         if isinstance(val, xbmc.InfoTagVideo):
             upnext_info[key] = {
                 'episodeid': val.getDbId(),
                 # Use show title as substitute for missing ListItem tvshowid
-                'tvshowid': val.getTVShowTitle() or -1,
+                'tvshowid': tvshowid or val.getTVShowTitle() or -1,
                 'title': val.getTitle(),
                 'art': {
                     'thumb': thumb,
