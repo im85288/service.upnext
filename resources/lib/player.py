15a4a7b5977f3d886b99db13e44129a5e189149b
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2021-01-31 14:10:06 +1100

    Revert "Proper fix for arguments-differ pylint warning"
    This reverts commit 29b8c27a7244e3515db9973a51cedcecd2643728.
    
    Issue still exists in Pylint 1.9.5

--- resources/lib/player.py
+++ resources/lib/player.py
@@ -164,7 +164,7 @@ class UpNextPlayer(xbmc.Player):
             self.state.speed = 0
         return self.state.speed
 
-    def getTime(self, **kwargs):  # pylint: disable=invalid-name
+    def getTime(self, use_infolabel=False):  # pylint: disable=invalid-name, arguments-differ
         # Use current stored value if playing forced
         if self.state.playing and not self.state.actual('playing'):
             actual = self.state.time
@@ -172,7 +172,7 @@ class UpNextPlayer(xbmc.Player):
         else:
             actual = (
                 utils.time_to_seconds(xbmc.getInfoLabel('VideoPlayer.Time'))
-                if kwargs.get('use_infolabel')
+                if use_infolabel
                 else getattr(xbmc.Player, 'getTime')(self)
             )
         self.state.time = actual
