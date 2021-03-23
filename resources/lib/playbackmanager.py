5694ddaa99a2a5e9eb1e29969f889b347281c9b2
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2021-03-23 23:16:12 +1100

    Split and reorganise playbackmanager methods

--- resources/lib/playbackmanager.py
+++ resources/lib/playbackmanager.py
@@ -26,39 +26,6 @@ class UpNextPlaybackManager(object):  # pylint: disable=useless-object-inheritan
     def log(cls, msg, level=utils.LOGINFO):
         utils.log(msg, name=cls.__name__, level=level)
 
-    def start(self, called=[False]):  # pylint: disable=dangerous-default-value
-        # Exit if playbackmanager previously requested
-        if called[0]:
-            return False
-        # Stop any existing playbackmanager
-        self.stop()
-        called[0] = True
-
-        next_item, source = self.state.get_next()
-        # No next item to play, get out of here
-        if not next_item:
-            self.log('Exiting: no next item to play')
-            called[0] = False
-            return False
-
-        # Show popup and get new playback state
-        play_next, keep_playing = self.run(next_item, source)
-        self.state.playing_next = play_next
-
-        # Dequeue and stop playback if not playing next file
-        if not play_next and self.state.queued:
-            self.state.queued = api.dequeue_next_item()
-        if not keep_playing:
-            self.log('Stopping playback')
-            self.player.stop()
-        # Relaunch popup if shuffle enabled to get new random episode
-        elif self.state.shuffle and not play_next:
-            called[0] = False
-            return self.start()
-
-        called[0] = False
-        return True
-
     def create_popup(self, next_item, source=None):
         # Only use Still Watching? popup if played limit has been reached
         if self.state.played_limit:
@@ -91,131 +58,7 @@ class UpNextPlaybackManager(object):  # pylint: disable=useless-object-inheritan
 
         return self.get_popup_state(show_upnext=show_upnext)
 
-    def run(self, next_item, source=None):
-        self.log('Started')
-        self.running = True
-
-        # Add next file to playlist if existing playlist is not being used
-        if self.state.enable_queue and source != 'playlist':
-            self.state.queued = api.queue_next_item(self.state.data, next_item)
-
-        # Create Kodi dialog to show UpNext or Still Watching? popup
-        popup_state = self.create_popup(next_item, source)
-        # Show popup and update state of controls
-        popup_state = self.show_popup_and_wait(popup_state)
-        # Close dialog once we are done with it
-        self.remove_popup()
-
-        # Update played in a row count if auto_play otherwise reset
-        self.state.played_in_a_row = (
-            self.state.played_in_a_row + 1 if popup_state['auto_play'] else 1
-        )
-
-        # Update shuffle state
-        self.state.shuffle = popup_state['shuffle']
-
-        # Signal to Trakt that current item has been watched
-        utils.event(
-            message='NEXTUPWATCHEDSIGNAL',
-            data={'episodeid': self.state.episodeid},
-            encoding='base64'
-        )
-
-        # Popup closed prematurely
-        if not popup_state['done']:
-            self.log('Exiting: popup force closed')
-
-        # Shuffle start request
-        elif popup_state['shuffle_start']:
-            self.log('Exiting: shuffle requested')
-            popup_state['done'] = False
-
-        elif not (popup_state['auto_play'] or popup_state['play_now']):
-            self.log('Exiting: playback not selected')
-            popup_state['done'] = False
-
-        if not popup_state['done']:
-            # Reset signals
-            self.sigstop = False
-            self.sigterm = False
-            self.running = False
-
-            play_next = False
-            # Stop playing if Stop button was clicked on popup, or if Still
-            # Watching? popup was shown (to prevent unwanted playback that can
-            # occur if fast forwarding through popup), or if starting shuffle
-            keep_playing = (
-                not popup_state['stop']
-                and (
-                    popup_state['show_upnext']
-                    or popup_state['shuffle_start']
-                )
-            )
-            return play_next, keep_playing
-
-        # Request playback of next file based on source and type
-        has_addon_data = self.state.has_addon_data()
-        # Primary method is to play next playlist item
-        if source == 'playlist' or self.state.queued:
-            # Can't just seek to end of file as this triggers inconsistent Kodi
-            # behaviour:
-            # - Will sometimes continue playing past the end of the file
-            #   preventing next file from playing
-            # - Will sometimes play the next file correctly then play it again
-            #   resulting in loss of UpNext state
-            # - Will sometimes play the next file immediately without
-            #   onPlayBackStarted firing resulting in tracking not activating
-            # - Will sometimes work just fine
-            # Can't just wait for next file to play as VideoPlayer closes all
-            # video threads when the current file finishes
-            if popup_state['play_now'] or popup_state['play_on_cue']:
-                api.play_playlist_item(
-                    # Use previously stored next playlist position if available
-                    position=next_item.get('playlist_position', 'next'),
-                    resume=self.state.enable_resume
-                )
-
-        # Fallback addon playback method, used if addon provides play_info
-        elif has_addon_data:
-            api.play_addon_item(
-                self.state.data,
-                self.state.encoding,
-                self.state.enable_resume
-            )
-
-        # Fallback library playback method, not normally used
-        else:
-            api.play_kodi_item(next_item, self.state.enable_resume)
-
-        # Determine playback method. Used for logging purposes
-        self.log('Playback requested: using{0}{1}{2} method'.format(
-            # Playback action type
-            ' play_now' if popup_state['play_now'] else
-            ' auto_play_on_cue' if popup_state['play_on_cue'] else
-            ' auto_play',
-            # Item information source
-            ' play_url' if (has_addon_data == 2) else
-            ' play_info' if (has_addon_data == 3) else
-            ' missing_addon_data' if (has_addon_data == 1) else
-            ' library' if (utils.get_int(next_item, 'episodeid') != -1) else
-            ' file',
-            # Playback method
-            ' playlist' if source == 'playlist' else
-            ' queue' if self.state.queued else
-            ' direct'
-        ), utils.LOGDEBUG)
-
-        # Reset signals
-        self.log('Stopped')
-        self.sigstop = False
-        self.sigterm = False
-        self.running = False
-
-        play_next = True
-        keep_playing = True
-        return play_next, keep_playing
-
-    def show_popup_and_wait(self, popup_state):
+    def display_popup(self, popup_state):
         # Get video details, exit if no video playing
         with self.player as check_fail:
             total_time = self.player.getTotalTime()
@@ -320,14 +163,57 @@ class UpNextPlaybackManager(object):  # pylint: disable=useless-object-inheritan
             return default_state
         return current_state
 
-    def show_popup(self):
-        if not self.popup:
-            return False
+    def play_next_video(self, next_item, source, popup_state):
+        has_addon_data = self.state.has_addon_data()
+        # Primary method is to play next playlist item
+        if source == 'playlist' or self.state.queued:
+            # Can't just seek to end of file as this triggers inconsistent Kodi
+            # behaviour:
+            # - Will sometimes continue playing past the end of the file
+            #   preventing next file from playing
+            # - Will sometimes play the next file correctly then play it again
+            #   resulting in loss of UpNext state
+            # - Will sometimes play the next file immediately without
+            #   onPlayBackStarted firing resulting in tracking not activating
+            # - Will sometimes work just fine
+            # Can't just wait for next file to play as VideoPlayer closes all
+            # video threads when the current file finishes
+            if popup_state['play_now'] or popup_state['play_on_cue']:
+                api.play_playlist_item(
+                    # Use previously stored next playlist position if available
+                    position=next_item.get('playlist_position', 'next'),
+                    resume=self.state.enable_resume
+                )
 
-        with self.popup:
-            self.popup.show()
-            utils.set_property('service.upnext.dialog', 'true')
-            return True
+        # Fallback addon playback method, used if addon provides play_info
+        elif has_addon_data:
+            api.play_addon_item(
+                self.state.data,
+                self.state.encoding,
+                self.state.enable_resume
+            )
+
+        # Fallback library playback method, not normally used
+        else:
+            api.play_kodi_item(next_item, self.state.enable_resume)
+
+        # Determine playback method. Used for logging purposes
+        self.log('Playback requested: using{0}{1}{2} method'.format(
+            # Playback action type
+            ' play_now' if popup_state['play_now'] else
+            ' auto_play_on_cue' if popup_state['play_on_cue'] else
+            ' auto_play',
+            # Item information source
+            ' play_url' if (has_addon_data == 2) else
+            ' play_info' if (has_addon_data == 3) else
+            ' missing_addon_data' if (has_addon_data == 1) else
+            ' library' if (utils.get_int(next_item, 'episodeid') != -1) else
+            ' file',
+            # Playback method
+            ' playlist' if source == 'playlist' else
+            ' queue' if self.state.queued else
+            ' direct'
+        ), utils.LOGDEBUG)
 
     def remove_popup(self):
         if not self.popup:
@@ -339,6 +225,123 @@ class UpNextPlaybackManager(object):  # pylint: disable=useless-object-inheritan
 
         del self.popup
         self.popup = None
+
+    def run(self, next_item, source=None):
+        self.log('Started')
+        self.running = True
+
+        # Add next file to playlist if existing playlist is not being used
+        if self.state.enable_queue and source != 'playlist':
+            self.state.queued = api.queue_next_item(self.state.data, next_item)
+
+        # Create Kodi dialog to show UpNext or Still Watching? popup
+        popup_state = self.create_popup(next_item, source)
+        # Display popup and update state of controls
+        popup_state = self.display_popup(popup_state)
+        # Close dialog once we are done with it
+        self.remove_popup()
+
+        # Update played in a row count if auto_play otherwise reset
+        self.state.played_in_a_row = (
+            self.state.played_in_a_row + 1 if popup_state['auto_play'] else 1
+        )
+
+        # Update shuffle state
+        self.state.shuffle = popup_state['shuffle']
+
+        # Signal to Trakt that current item has been watched
+        utils.event(
+            message='NEXTUPWATCHEDSIGNAL',
+            data={'episodeid': self.state.episodeid},
+            encoding='base64'
+        )
+
+        # Popup closed prematurely
+        if not popup_state['done']:
+            self.log('Exiting: popup force closed')
+
+        # Shuffle start request
+        elif popup_state['shuffle_start']:
+            self.log('Exiting: shuffle requested')
+            popup_state['done'] = False
+
+        elif not (popup_state['auto_play'] or popup_state['play_now']):
+            self.log('Exiting: playback not selected')
+            popup_state['done'] = False
+
+        if not popup_state['done']:
+            # Reset signals
+            self.sigstop = False
+            self.sigterm = False
+            self.running = False
+
+            play_next = False
+            # Stop playing if Stop button was clicked on popup, or if Still
+            # Watching? popup was shown (to prevent unwanted playback that can
+            # occur if fast forwarding through popup), or if starting shuffle
+            keep_playing = (
+                not popup_state['stop']
+                and (
+                    popup_state['show_upnext']
+                    or popup_state['shuffle_start']
+                )
+            )
+            return play_next, keep_playing
+
+        # Request playback of next file based on source and type
+        self.play_next_video(next_item, source, popup_state)
+
+        # Reset signals
+        self.log('Stopped')
+        self.sigstop = False
+        self.sigterm = False
+        self.running = False
+
+        play_next = True
+        keep_playing = True
+        return play_next, keep_playing
+
+    def show_popup(self):
+        if not self.popup:
+            return False
+
+        with self.popup:
+            self.popup.show()
+            utils.set_property('service.upnext.dialog', 'true')
+            return True
+
+    def start(self, called=[False]):  # pylint: disable=dangerous-default-value
+        # Exit if playbackmanager previously requested
+        if called[0]:
+            return False
+        # Stop any existing playbackmanager
+        self.stop()
+        called[0] = True
+
+        next_item, source = self.state.get_next()
+        # No next item to play, get out of here
+        if not next_item:
+            self.log('Exiting: no next item to play')
+            called[0] = False
+            return False
+
+        # Show popup and get new playback state
+        play_next, keep_playing = self.run(next_item, source)
+        self.state.playing_next = play_next
+
+        # Dequeue and stop playback if not playing next file
+        if not play_next and self.state.queued:
+            self.state.queued = api.dequeue_next_item()
+        if not keep_playing:
+            self.log('Stopping playback')
+            self.player.stop()
+        # Relaunch popup if shuffle enabled to get new random episode
+        elif self.state.shuffle and not play_next:
+            called[0] = False
+            return self.start()
+
+        called[0] = False
+        return True
 
     def stop(self, terminate=False):
         if terminate:
