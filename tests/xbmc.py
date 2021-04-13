f59ab09804e77790c9f868f5761bfcba2e23cc4d
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2021-04-13 13:14:08 +1000

    Make the NotifyAll stub call onNotification in a new thread to better simulate normal callback operation

--- tests/xbmc.py
+++ tests/xbmc.py
@@ -434,11 +434,11 @@ def _jsonrpc_notifyall(params):
     for ref in _monitor_instances.valuerefs():
         notification_handler = getattr(ref(), "onNotification", None)
         if callable(notification_handler):
-            notification_handler(
+            threading.Thread(target=notification_handler, args=(
                 params.get('sender'),
                 params.get('message'),
                 json.dumps(params.get('data'))
-            )
+            ))
 
     return True
 
