im85288/service.upnext

commit 2efec9ac5922c25699e8350d2453edab7cb07042

Simplify playlist handling
Use existing method to get now playing info from a playlist

resources/lib/api.py
@@ -186,8 +186,8 @@ def get_playlist_position():
     return None
 
 
-def get_next_in_playlist(position, unwatched_only=False):
-    """Function to get details of next episode in playlist"""
+def get_from_playlist(position, unwatched_only=False):
+    """Function to get details of item in playlist"""
 
     result = utils.jsonrpc(
         method='Playlist.GetItems',
@@ -203,7 +203,7 @@ def get_next_in_playlist(position, unwatched_only=False):
     )
     items = result.get('result', {}).get('items')
 
-    # Get first unwatched item in the list of remaining playlist entries
+    # Get first unwatched item in the list of playlist entries
     if unwatched_only and items:
         position_offset, item = next(
             (
@@ -213,13 +213,14 @@ def get_next_in_playlist(position, unwatched_only=False):
             (0, None)
         )
         position += position_offset
-    # Or just get the first item in the list of remaining playlist entries
+    # Or just get the first item in the list of playlist entries
     else:
         item = items[0] if items else None
 
-    # Don't check if next item is an episode, just use it if it is there
+    # Don't check if item is an episode, just use it if it is there
     if not item:  # item.get('type') != 'episode':
-        log('No next item found in playlist', utils.LOGWARNING)
+        log('No item in playlist at position {0}'.format(position),
+            utils.LOGWARNING)
         return None
 
     # Playlist item may not have had video info details set
@@ -231,17 +232,18 @@ def get_next_in_playlist(position, unwatched_only=False):
         utils.get_int(item, 'id')
     )
     item['tvshowid'] = utils.get_int(item, 'tvshowid')
+    item['showtitle'] = item.get('showtitle', constants.MIXED_PLAYLIST)
     # If missing season/episode, change to empty string to avoid episode
     # formatting issues ("S-1E-1") in UpNext popup
     if utils.get_int(item, 'season') == constants.UNDEFINED:
         item['season'] = ''
     if utils.get_int(item, 'episode') == constants.UNDEFINED:
-        item['episode'] = ''
+        item['episode'] = position
 
     # Store current playlist position for later use
     item['playlist_position'] = position
 
-    log('Next item in playlist at position {0}: {1}'.format(position, item))
+    log('Item in playlist at position {0}: {1}'.format(position, item))
     return item
 
 

resources/lib/state.py
@@ -119,7 +119,7 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
 
         # Next item from non-plugin playlist
         elif playlist_position and not self.shuffle_on:
-            next_item = api.get_next_in_playlist(
+            next_item = api.get_from_playlist(
                 playlist_position,
                 SETTINGS.unwatched_only
             )
@@ -244,16 +244,8 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
             source = constants.PLUGIN_TYPES[plugin_type]
 
         elif playlist_position:
-            current_item = api.get_now_playing()
+            current_item = api.get_from_playlist(playlist_position - 1)
             source = 'playlist'
-
-            if current_item:
-                if not current_item.get('showtitle'):
-                    current_item['showtitle'] = '_playlist'
-                if current_item.get('season') == constants.UNDEFINED:
-                    current_item['season'] = 0
-                if current_item.get('episode') == constants.UNDEFINED:
-                    current_item['episode'] = playlist_position
 
         elif media_type == 'episode':
             current_item = self._get_library_now_playing()

resources/lib/constants.py
@@ -46,6 +46,8 @@ LOG_ENABLE_DISABLED = 0
 LOG_ENABLE_INFO = 1
 LOG_ENABLE_DEBUG = 2
 
+MIXED_PLAYLIST = '_mixedplaylist'
+
 PLUGIN_TYPES = (
     'plugin_data_error',
     'plugin_playlist',

resources/lib/detector.py
@@ -62,7 +62,7 @@ class UpNextHashStore(object):
             return False
 
         # Playlist with no episode details
-        if for_saving and self.seasonid[:len('_playlist')] == '_playlist':
+        if for_saving and self.seasonid.startswith(constants.MIXED_PLAYLIST):
             return False
 
         # No new episode details, assume current hashes are still valid

commit a6898414091fbbbf57500d2004a3914f15112b92

Partially revert 2efec9a
Caused regressions in handling playlists of non-library content

resources/lib/api.py
@@ -232,13 +232,12 @@ def get_from_playlist(position, unwatched_only=False):
         utils.get_int(item, 'id')
     )
     item['tvshowid'] = utils.get_int(item, 'tvshowid')
-    item['showtitle'] = item.get('showtitle', constants.MIXED_PLAYLIST)
     # If missing season/episode, change to empty string to avoid episode
     # formatting issues ("S-1E-1") in UpNext popup
     if utils.get_int(item, 'season') == constants.UNDEFINED:
         item['season'] = ''
     if utils.get_int(item, 'episode') == constants.UNDEFINED:
-        item['episode'] = position
+        item['episode'] = ''
 
     # Store current playlist position for later use
     item['playlist_position'] = position

resources/lib/state.py
@@ -247,6 +247,14 @@ class UpNextState(object):  # pylint: disable=too-many-public-methods
             current_item = api.get_from_playlist(playlist_position - 1)
             source = 'playlist'
 
+            if current_item:
+                if not current_item.get('showtitle'):
+                    current_item['showtitle'] = constants.MIXED_PLAYLIST
+                if not current_item['season']:
+                    current_item['season'] = 0
+                if not current_item['episode']:
+                    current_item['episode'] = playlist_position
+
         elif media_type == 'episode':
             current_item = self._get_library_now_playing()
             source = 'library'